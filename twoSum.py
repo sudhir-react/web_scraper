b21b8336b80300aa536c12aceee6a07b7049a52d
sudhiripp@hotmail.com <sudhiripp@hotmail.com>
2026-09-10 19:33:43 +0530

    twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,23 @@
+# def two_sum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
+
+
+# print(two_sum([2, 11, 7, 15], 9))
+
+
+
+def two_sum(nums, target):
+    seen = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in seen:
+            return [seen[complement], i]
+
+        seen[num] = i
+
+print(two_sum([2, 7, 11, 15], 9))
